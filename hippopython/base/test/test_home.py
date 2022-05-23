6bfe5ed28abccf16d6d543ae633ed99012e3e460
michel4alves <michelsantosa@gmail.com>
2022-05-23 13:00:55 -0300

    configurações na home.html part 1

--- hippopython/base/test/test_home.py
+++ hippopython/base/test/test_home.py
@@ -1,6 +1,23 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+
+from hippopython.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
-    assert resp.status_code == 200
+@pytest.fixture
+def resposta(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resposta):
+    assert resposta.status_code == 200
+
+
+def test_title(resposta):
+    assert_contains(resposta, '<title>hippopython - Curso Django</title>')
+
+
+def test_home_link(resposta):
+    assert_contains(resposta, f'href="{reverse("base:home")}">hippopython</a>')
