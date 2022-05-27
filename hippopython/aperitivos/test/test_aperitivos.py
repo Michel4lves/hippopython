fb2dd0c156f8f5203dc9c55577747cc51aec3bd4
michel4alves <michelsantosa@gmail.com>
2022-05-26 22:26:43 -0300

    criada a app aperitivos

--- hippopython/aperitivos/test/test_aperitivos.py
+++ hippopython/aperitivos/test/test_aperitivos.py
@@ -1,6 +1,8 @@
 import pytest
 from django.urls import reverse
 
+from hippopython.django_assertions import assert_contains
+
 
 @pytest.fixture
 def resp(client):
@@ -9,3 +11,13 @@ def resp(client):
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_titulo_video(resp):
+    assert_contains(resp, '<h1 class="card-header">Vídeo Aperitivo: Motivação</h1>')
+
+
+def test_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/700105282?h=9948754776&amp;'
+                          'badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"'
+                    )
