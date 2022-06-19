a27c555d43c8d224aaabaadb910b85ff9eba732e
michel4alves <michelsantosa@gmail.com>
2022-06-19 00:55:52 -0300

    Implementado recuperação de senha (sem templates) Configuração de envio de emails para produção (correção de envio de email API)

--- hippopython/settings.py
+++ hippopython/settings.py
@@ -101,7 +101,6 @@ EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-SENDGRID_API_KEY = config('SENDGRID_API_KEY')
 
 
 # Configuração Djando Debug Tollbar
